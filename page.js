5141dbd3cb4d34568775fb5ee86d35b987c5b4f9
KiwiNotKiwi <kiwinotkiwi@gmail.com>
2017-10-20 11:36:23 -0700

    Update page.js

--- page.js
+++ page.js
@@ -8,20 +8,13 @@ var main = "<div class='main'> <div class='scroller sexy'> </div> </div>"
 var blogentry = "<bentry> <bheader> <btitle>owo owo owo owo owo owo owo owo owo owo owo owo owo owo owo owo </btitle> <btimestamp> <bdate>10.19.2017</bdate> <btime>8:40am (PST)</btime> </btimestamp> </bheader> <bbody> <bp>This site is mainly idek. I guess to shove update information and also my random ass projects onto. You can mostly ignore everything here as it probably wont help you with anything.</bp> <bp>But If you want to see cancer then by all means stay as everything I do is absolute shit. Enjoy.</bp> </bbody> </bentry>" + 
     "<bentry> <bheader> <btitle>owo owo owo owo owo owo owo owo owo owo owo owo owo owo owo owo </btitle> <btimestamp> <bdate>10.20.2017</bdate> <btime>10:19am (PST)</btime> </btimestamp> </bheader> <bbody> <bp>This site is mainly idek. I guess to shove update information and also my random ass projects onto. You can mostly ignore everything here as it probably wont help you with anything.</bp> <bp>But If you want to see cancer then by all means stay as everything I do is absolute shit. Enjoy.</bp> </bbody> </bentry>"
 
-
-
-$(function() {
-  $( "#page" ).prepend( menubtn + menu );
-  $( "#page" ).append( main + appearance );
-  $( ".sexy" ).prepend( blogentry );
-  return required();
-});
-
-function required(){
-$( ".menubtn" ).click(function() {
+function menubutton(){
+  $( ".menubtn" ).click(function() {
   $( ".menubtn > span" ).toggleClass( "active" );
   $( ".menu" ).toggleClass( "active" );
 });
+}
+function appearancetoggle(){
 $( ".aptoggle" ).click(function() {
   $( "body" ).toggleClass( "bgtoggle" );
 });
