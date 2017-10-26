4342619bb22603643a439d9eb80c98eb982d0c42
KiwiNotKiwi <kiwinotkiwi@gmail.com>
2017-10-26 12:04:30 -0700

    Update global.js

--- global.js
+++ global.js
@@ -4,7 +4,7 @@ var appearance =
   "<label class='appearance'> <input type='checkbox'> <span class='aptoggle'></span> <div class='imlazytoggle'>plz no moar</div> </label>";
 
 var menu =
-  "<div class='menu'> <div class='scroller'> <nheader>Internal</nheader> <nsection> <a href='#'>Docile Memes</a> <a href='#'>Themes</a> <a href='#'>UI Projects</a> </nsection> <nheader>External</nheader> <nsection> <a href='#'>Edgy Memes</a> </nsection> <nheader>Contact</nheader> <nsection> <a href='#'>Steam</a> <a href='#'>Discord</a> <a href='#'>Twitter</a> <a href='#'>Youtube</a> <a href='#'>Snapchat</a> </nsection> </div> </div>";
+  "<div class='menu'> <div class='scroller'> <nheader>Internal</nheader> <nsection> <a href='#'>Docile Memes</a> <a href='meme.cx/themes'>Themes</a> <a href='#'>UI Projects</a> </nsection> <nheader>External</nheader> <nsection> <a href='#'>Edgy Memes</a> </nsection> <nheader>Contact</nheader> <nsection> <a href='#'>Steam</a> <a href='#'>Discord</a> <a href='#'>Twitter</a> <a href='#'>Youtube</a> <a href='#'>Snapchat</a> </nsection> </div> </div>";
 //Link list like blog menu
 var main = "<div class='main'> <div class='scroller sexy'> </div> </div>";
 
