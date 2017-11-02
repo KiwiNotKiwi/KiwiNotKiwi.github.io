d16a49d41c1fbf57618b5bdd50cb50b629c6cfdf
KiwiNotKiwi <kiwinotkiwi@gmail.com>
2017-11-02 09:05:41 -0700

    Update page.js

--- page.js
+++ page.js
@@ -1,2 +1,1 @@
-var sitenav = "<header>Internal</header> <a href='#'>lol</a> <header>External</header> <a href='#'>lol</a> <header>Contact</header> <a href='#'>lol</a>";
 var blog = "<section> <header>oWo OwO owO Owo<span>10.26.2017 <b>12:56pm (PST)</b></span></header> <article>fite me m8</article> </section>";
