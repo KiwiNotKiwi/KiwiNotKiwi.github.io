04328467d8aace6545521b3a4cdbf412e34323a5
KiwiNotKiwi <kiwinotkiwi@gmail.com>
2017-11-01 18:10:00 -0700

    Update global.js

--- global.js
+++ global.js
@@ -1,21 +1,34 @@
-var menubtn =
-  "<div class='menubtn'><span></span><span></span><span></span></div>";
-var appearance =
-  "<label class='appearance'> <input type='checkbox'> <span class='aptoggle'></span> <div class='imlazytoggle'>plz no moar</div> </label>";
+var nav = "<nav> <div class='scroller'>  </div> </nav>";
+var main = "<main> <div class='scroller'> </div> </main>";
+var menubutton = "<div class='menubtn'><div><span></span><span></span><span></span></div></div>";
+var backgroundtoggle = "<label class='bgtoggle tip left' ttip='pls no moar'> <input type='checkbox'><span></span></label>";
+var tooltipe = "<div class='tooltip'></div>";
 
-var menu =
-  "<div class='menu'> <div class='scroller'> <nheader>Internal</nheader> <nsection> <a href='#'>Docile Memes</a> <a href='http://meme.cx/themes/'>Themes</a> <a href='#'>UI Projects</a> </nsection> <nheader>External</nheader> <nsection> <a href='#'>Edgy Memes</a> </nsection> <nheader>Contact</nheader> <nsection> <a href='#'>Steam</a> <a href='#'>Discord</a> <a href='#'>Twitter</a> <a href='#'>Youtube</a> <a href='#'>Snapchat</a> </nsection> </div> </div>";
-//Link list like blog menu
-var main = "<div class='main'> <div class='scroller sexy'> </div> </div>";
-
-function menubutton() {
+function menubtn() {
   $(".menubtn").click(function() {
-    $(".menubtn > span").toggleClass("active");
-    $(".menu").toggleClass("active");
+    $("nav").toggleClass("active");
+    $(".menubtn").toggleClass("active");
   });
 }
-function appearancetoggle() {
-  $(".aptoggle").click(function() {
-    $("body").toggleClass("bgtoggle");
+function bgtoggle() {
+  $(".bgtoggle span").click(function() {
+    $("body").toggleClass("active");
+    $(".bgtoggle").toggleClass("active");
   });
+}
+function tooltipero() {
+$(".tip.left").hover(function(){
+  var tooltp = $(this).attr("ttip");
+  var showtip = $(".tooltip");
+    $('.tooltip').html( tooltp );
+    showtip.css({ 
+        display: "block",
+        left: ($(this).offset().left - $(".tooltip").width() - "24") + "px",
+        top: $(this).offset().top + "px"
+    });
+},
+function(){
+    $(".tooltip").hide();   
+    $('.tooltip').html("");
+});
 }
