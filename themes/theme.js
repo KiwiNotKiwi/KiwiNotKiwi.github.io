20dd7e85b3307b995ee425ad66cf53536a791484
KiwiNotKiwi <kiwinotkiwi@gmail.com>
2017-11-09 09:15:07 -0800

    Create theme.js

---
+++ themes/theme.js
@@ -0,0 +1,10 @@
+var link = '';
+var header = 'straWberry';
+var description = 'super strawberry lanA';
+var tags = '#fruit #cool #yay';
+
+var previewinfo = '<div class="pheader"><header>' + header + '</header><section><span>' + description + '</span><span>' + tags + '</span></section></div>';
+
+var demo = '<div class="framewrap"><iframe id="livepreview" class="liveview" src="https://betterdocs.net/themes/assets/darkdiscord.html?theme=https://raw.githubusercontent.com/0mniscient/Discord-Themes/master/Themes/Quiet.theme.css" width="100%" height="100%" allowfullscreen="" frameborder="0"></iframe></div>';
+
+var comments = '';
