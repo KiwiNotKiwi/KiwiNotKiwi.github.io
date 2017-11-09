e5b25cde553afc42f6fea0844d71348f9d2c9f8e
KiwiNotKiwi <kiwinotkiwi@gmail.com>
2017-11-09 09:16:39 -0800

    Update theme.js

--- themes/theme.js
+++ themes/theme.js
@@ -5,6 +5,6 @@ var tags = '#fruit #cool #yay';
 
 var previewinfo = '<div class="pheader"><header>' + header + '</header><section><span>' + description + '</span><span>' + tags + '</span></section></div>';
 
-var demo = '<div class="framewrap"><iframe id="livepreview" class="liveview" src="https://betterdocs.net/themes/assets/darkdiscord.html?theme=https://raw.githubusercontent.com/0mniscient/Discord-Themes/master/Themes/Quiet.theme.css" width="100%" height="100%" allowfullscreen="" frameborder="0"></iframe></div>';
+var demo = '<div class="framewrap"><iframe id="livepreview" class="liveview" src="https://betterdocs.net/themes/assets/darkdiscord.html" width="100%" height="100%" allowfullscreen="" frameborder="0"></iframe></div>';
 
 var comments = '';
