6eda5b721b6e1280a1d5a4198301a9dc0ea55ace
KiwiNotKiwi <kiwinotkiwi@gmail.com>
2017-11-13 13:35:22 -0800

    Update theme.js

--- themes/theme.js
+++ themes/theme.js
@@ -1,9 +1,3 @@
-var header = 'straWberry';
-var author = 'KixiOrKiwi';
-var description = 'super strawberry lanA';
-var tags = '#fruit #cool #yay';
-var link = 'https://raw.githubusercontent.com/KiwiNotKiwi/Themes/master/straWberry.theme.css';
-
 var previewinfo = '<div class="pheader"><header>' + header + '</header><span>' + author + '</span><span>' + description + '</span><span>' + tags + '</span></><a href="' + link + '" download>Download</a></div>';
 
 var demo = '<div class="framewrap"><iframe id="livepreview" class="liveview" src="https://betterdocs.net/themes/assets/darkdiscord.html?theme=https://raw.githubusercontent.com/KiwiNotKiwi/Themes/master/straWberry.theme.css" width="100%" height="100%" allowfullscreen="" frameborder="0"></iframe></div>';
