951501d83b1ac7323a5f54787b0c817c931be11f
KiwiNotKiwi <kiwinotkiwi@gmail.com>
2017-11-13 13:32:08 -0800

    Update theme.js

--- themes/theme.js
+++ themes/theme.js
@@ -6,6 +6,6 @@ var link = 'https://raw.githubusercontent.com/KiwiNotKiwi/Themes/master/straWber
 
 var previewinfo = '<div class="pheader"><header>' + header + '</header><span>' + author + '</span><span>' + description + '</span><span>' + tags + '</span></><a href="' + link + '" download>Download</a></div>';
 
-var demo = '<div class="framewrap"><iframe id="livepreview" class="liveview" src="https://betterdocs.net/themes/assets/darkdiscord.html?theme=https://raw.githubusercontent.com/0mniscient/Discord-Themes/master/Themes/Quiet.theme.css" width="100%" height="100%" allowfullscreen="" frameborder="0"></iframe></div>';
+var demo = '<div class="framewrap"><iframe id="livepreview" class="liveview" src="https://betterdocs.net/themes/assets/darkdiscord.html?theme=https://raw.githubusercontent.com/KiwiNotKiwi/Themes/master/straWberry.theme.css" width="100%" height="100%" allowfullscreen="" frameborder="0"></iframe></div>';
 
 var comments = '';
