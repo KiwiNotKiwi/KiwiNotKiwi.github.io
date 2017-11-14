fc99eb41e63fc5c50f1f24ea3fb3710e858e6ec0
KiwiNotKiwi <kiwinotkiwi@gmail.com>
2017-11-13 17:22:56 -0800

    Update theme.js

--- themes/theme.js
+++ themes/theme.js
@@ -1,5 +1,5 @@
 var previewinfo = '<div class="pheader"><header>' + header + '</header><span>' + author + '</span><span>' + description + '</span><span>' + tags + '</span></><a href="' + link + '" download>Download</a></div>';
 
-var demo = '<div class="framewrap"><iframe id="livepreview" class="liveview" src="https://betterdocs.net/themes/assets/darkdiscord.html?theme=' + link + '" width="100%" height="100%" allowfullscreen="" frameborder="0"></iframe></div>';
+var demo = '<div class="framewrap"><iframe id="livepreview" class="liveview" src="https://meme.cx/themes/discorddemo/discord.html?theme=' + link + '" width="100%" height="100%" allowfullscreen="" frameborder="0"></iframe></div>';
 
 var comments = '';
