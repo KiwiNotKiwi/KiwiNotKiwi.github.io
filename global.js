f85efea6bb64b33784b75fea07fda6149768ec03
KixiOrKiwi <kiwinotkiwi@gmail.com>
2017-12-06 10:59:00 -0800

    Update global.js

--- global.js
+++ global.js
@@ -1,36 +1,62 @@
-var nav = "<nav> <div class='scroller'>  </div> </nav>";
-var main = "<main> <div class='scroller'> </div> </main>";
-var menubutton = "<div class='menubtn'><div><span></span><span></span><span></span></div></div>";
-var backgroundtoggle = "<div class='bgtoggle left' ttip='pls no moar'><span></span></div>";
-var tooltipe = "<div class='tooltip'></div>";
-
-var sitenav = "<a href='http://meme.cx/'>Home</a><header>Internal</header> <a href='http://meme.cx/themes/'>Themes</a> <!--<header>External</header> <a href='#'>lol</a> <header>Contact</header> <a href='#'>lol</a>-->";
-
-function menubtn() {
-  $(".menubtn").click(function() {
-    $("nav").toggleClass("active");
-    $(".menubtn").toggleClass("active");
-  });
+var internal = [
+{
+	navname: `To Be`,
+	navaddress: `#`
 }
-function bgtoggle() {
-  $(".bgtoggle").click(function() {
-    $("body").toggleClass("active");
-    $(".bgtoggle").toggleClass("active");
-  });
+]
+
+var external = [
+{
+	navname: `Other Discords`,
+	navaddress: `#`
 }
-function tooltipero() {
-$(".tip.left").hover(function(){
-  var tooltp = $(this).attr("ttip");
-  var showtip = $(".tooltip");
-    $('.tooltip').html( tooltp );
-    showtip.css({ 
-        display: "block",
-        left: ($(this).offset().left - $(".tooltip").width() - "24") + "px",
-        top: $(this).offset().top + "px"
-    });
+]
+
+var personal = [
+{
+	navname: `Discord`,
+	navaddress: `#`
 },
-function(){
-    $(".tooltip").hide();   
-    $('.tooltip').html("");
-});
+{
+	navname: `Twitter`,
+	navaddress: `#`
+},
+{
+	navname: `Twitch`,
+	navaddress: `#`
+},
+{
+	navname: `Youtube`,
+	navaddress: `#`
+},
+{
+	navname: `Snapchat`,
+	navaddress: `#`
 }
+]
+
+var navone = ``
+var navtwo = ``
+var navthree = ``
+$.each(internal,function(index,item){navone +=`<span><a href="`+item.navaddress+`">`+item.navname+`</a></span>`})
+$.each(external,function(index,item){navtwo +=`<span><a href="`+item.navaddress+`">`+item.navname+`</a></span>`})
+$.each(personal,function(index,item){navthree +=`<span><a href="`+item.navaddress+`">`+item.navname+`</a></span>`})
+
+$(`nav .internal .lower`).html(navone)
+$(`nav .external .lower`).html(navtwo)
+$(`nav .personal .lower`).html(navthree)
+
+$(`.chngbtn`).click(function(){$(this).toggleClass(`active`)})
+
+$(`#sitenav`).click(function(){$(`nav`).toggleClass(`active`)})
+
+$(`#seizure`).click(function(){$(`#page`).toggleClass(`active`)})
+
+$(window).resize(function() {
+	if ($(window).width() < 620) {
+		
+	}
+	else {
+		
+	}
+})
